d0f2f68f15c7e36ce4d860dbffa3114908ccd681
Diego Zúñiga <15971140+diejel@users.noreply.github.com>
2022-09-06 15:48:00 -0300

    Add files via upload

---
+++ amdocs_challenge.py
@@ -0,0 +1,166 @@
+import pywebio,re
+import asyncio
+from pywebio import start_server
+from pywebio import *
+import pywebio
+from pywebio.output import *
+from pywebio.input import *
+from subprocess import *
+from pywebio.session import defer_call, info as session_info, run_async
+from pywebio.session import *
+from pywebio.pin import *
+# Read the content of the file that shows all namespaces
+path_folder = '/home/dcubaz/Documents/pywebio/pyweb'
+running_status = 'https://docs.emlid.com/edge/img/led-status/status_led_green_blinking.gif'
+error_status = 'https://digitale22.files.wordpress.com/2014/01/exc401.gif'
+pending_status = 'https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif'
+succeded_status = 'https://image.pngaaa.com/354/140354-middle.png'
+unknown_status = 'https://static-00.iconduck.com/assets.00/document-unknown-icon-440x512-y6souw0o.png'
+warn_status = 'https://icons.iconarchive.com/icons/3dlb/3d-vol2/256/warning-icon.png'
+pip_cmd =  "cat get_all_all_ns.txt"
+#pip_cmd = 'kubectl get all --all-namespaces'
+logs_cmd = "head -n {} /var/log/auth.log".format(10)
+ns_name = [ ]
+pd_name = [ ]
+msg_fail_pod = [ ]
+
+from functools import partial
+
+def row_action(choice, id):
+    put_text("You click %s button with id: %s" % (choice, id))
+
+#function get_shell_response
+def get_shell_response( input_cmd, path_cwd ):
+    out_cmd = check_output(input_cmd,shell=True,cwd=path_cwd)
+    format_out_cmd = out_cmd.decode('utf-8')
+    return format_out_cmd
+
+#fucntion for retrieve logs
+def print_log(out_msg,descr):
+    code = textarea(descr, code={ 'mode': "python",  # code language
+    'theme': 'cobalt', 
+     }, value=out_msg )
+    put_code(code, language='python')
+    #return code  
+
+
+#-----Start comment----
+#Firsr group:
+def  main():
+      
+        mat_str = get_shell_response(pip_cmd,path_folder)
+        out_cmd = mat_str.split("\n\r\n")
+        mat1 = out_cmd[0]
+        print(len(out_cmd))
+
+        for j in range(0,len(out_cmd)):
+            if "pod/" in out_cmd[j]:
+                print("existe pods")
+                put_markdown('# Pods Section')
+                mat = out_cmd[j]
+                mat_hor_lines = mat.split("\n")
+                mat_list = [ ]
+                for p in mat_hor_lines:
+                    arr = p.split()
+                    if arr[3] == "Running":
+                        arr.append(put_image(src=running_status,width='40px'))
+ 
+                    elif arr[3] == "Succeeded":
+                        arr.append(put_image(src=succeded_status,width='45px'))
+
+                    elif arr[3] == "Pending":
+                        arr.append(put_image(src=pending_status,width='30px'))
+
+                    elif arr[3] == "Failed":
+                        
+                        ns_name_val = arr[0]; pd_name_val = arr[1].split("/")[1]
+                        ns_name_val = ns_name.append(ns_name_val)
+                        pd_name_val = pd_name.append(pd_name_val)
+                        tshoot_pod = 'kubectl describe -n {} pod {}'.format(ns_name,pd_name)
+                        arr.append(put_image(src=error_status,width='65px'))
+
+                    elif arr[3] == "Unknown":
+                        arr.append(put_image(src=unknown_status,width='25px'))
+
+                    else:
+                        arr.append("MONITORING")
+                        #arr.append("LOGS")
+                    mat_list.append(arr)
+                put_table(mat_list)
+            elif "service/" in out_cmd[j]:
+                print("existe services")
+                put_markdown('# Services Section')
+                mat = out_cmd[j]
+                mat_hor_lines = mat.split("\n")
+                mat_list = [ ]
+                for p in mat_hor_lines:
+                    arr = p.split()
+                    mat_list.append(arr)
+                    
+                put_table(mat_list)
+            elif "daemonset.apps/" in out_cmd[j]:
+                print("existe daemonset.apps")
+                put_markdown('# DaemonSet Section')
+                mat = out_cmd[j]
+                mat_hor_lines = mat.split("\n")
+                mat_list = [ ]
+                for p in mat_hor_lines:
+                    arr = p.split()
+                    try:
+                        if int(arr[3]) < int(arr[2]):
+                            arr.insert( 2, put_image(src=warn_status,width='50px'))
+                        elif int(arr[3]) == int(arr[2]):
+                            arr.insert( 2, put_image(src=succeded_status,width='50px'))
+                    except:    
+                            arr.insert(2,"MONITORING")
+                    mat_list.append(arr)
+                put_table(mat_list)
+            elif "deployment.apps/" in out_cmd[j]:
+                print("existe deployment.apps")
+                put_markdown('# Deployment Section')
+                mat = out_cmd[j]
+                mat_hor_lines = mat.split("\n")
+                mat_list = [ ]
+                for p in mat_hor_lines:
+                    arr = p.split()
+                    mat_list.append(arr)
+                put_table(mat_list)
+            elif "replicaset.apps/" in out_cmd[j]:
+                print("existe replicaset.apps")
+                put_markdown('# Replicaset Section')
+                mat = out_cmd[j]
+                mat_hor_lines = mat.split("\n")
+                mat_list = [ ]
+                for p in mat_hor_lines:
+                    arr = p.split()
+                    try:
+                        if int(arr[3]) < int(arr[2]):
+                            arr.insert( 2, put_image(src=warn_status,width='50px'))
+                        elif int(arr[3]) == int(arr[2]):
+                            arr.insert( 2, put_image(src=succeded_status,width='50px'))
+                    except:    
+                            arr.insert(2,"MONITORING")
+                    mat_list.append(arr)
+                put_table(mat_list)
+            else:
+                print("no existe pods")
+                put_text("New Section")
+                put_markdown('# New Section')
+                mat = out_cmd[j]
+                mat_hor_lines = mat.split("\n")
+                mat_list = [ ]
+                for p in mat_hor_lines:
+                    arr = p.split()
+                    mat_list.append(arr)
+                put_table(mat_list)
+        #print_log(tshoot_pod,msg_fail_pod)
+        print(tshoot_pod)
+        for k in range(0,len(pd_name)):
+            print("namespace: {}, pod: {}".format(ns_name[k],pd_name[k]))
+            msg_fail_pod = "[Fail Alert!] Retrieving logs for pod:{}".format(pd_name[k])
+            tshoot_pod = 'kubectl describe -n {} pod {}'.format(ns_name[k],pd_name[k])
+            print_log(tshoot_pod,msg_fail_pod)
+
+if __name__ == '__main__':
+     start_server(main, debug=True, port=8080, cdn=False)
+     
